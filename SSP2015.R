2c8fbec5176fbc803a27c5343fe6da87136977a5
Atsushi Ishida <aishidajt9@gmail.com>
2016-10-29 16:28:33 +0900

    readspss

---
+++ SSP2015.R
@@ -0,0 +1,6 @@
+############################
+########## SSP2015 #########
+############################
+
+SSP2015<-foreign::read.spss("SSPI2015_20150923_ver1.sav",use.value.labels=F,to.data.frame=T,use.missings = T)
+attach(SSP2015)
